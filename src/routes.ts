20e07adc20c1f2cfc254a039c20d4abb666bc92d
Bruna <bruna.dev@hotmail.com>
2024-03-04 12:12:35 -0300

    Cadastrando novo cliente com nome, email e status

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,14 @@
 import { FastifyInstance, FastifyPluginOptions, FastifyRequest, FastifyReply } from "fastify";
+import { CreateCustomerController } from "./controllers/CreateCustomerController";
 
-export async function routes(fastify: FastifyInstance, options: FastifyPluginOptions ){
+export async function routes(fastify: FastifyInstance, options: FastifyPluginOptions) {
 
-    fastify.get("/teste", async (request: FastifyRequest, reply: FastifyReply ) => {
+    fastify.get("/teste", async (request: FastifyRequest, reply: FastifyReply) => {
         return { ok: true }
     })
+
+    fastify.post("/customer", async (request: FastifyRequest, reply: FastifyReply) => {
+        return new CreateCustomerController().handle(request, reply)
+    })
+    
 }
---
+++ src/services/CreateCustomerService.ts
@@ -0,0 +1,26 @@
+import prismaClient from "../prisma";
+
+interface CreateCustomerProps {
+    name: string;
+    email: string;
+}
+
+class CreateCustomerService {
+    async execute({ name, email }: CreateCustomerProps) {
+
+        if (!name || !email) {
+            throw new Error("Preencha todos os campos")
+        }
+
+        const customer = await prismaClient.customer.create({
+            data: {
+                name,
+                email,
+                status: true
+            }
+        })
+        return customer
+    }
+}
+
+export { CreateCustomerService }
