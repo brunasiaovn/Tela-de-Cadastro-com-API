b1089c2998a1bc9dd3c72de5a0cbb5c334bec7ac
Bruna <bruna.dev@hotmail.com>
2024-03-04 19:44:35 -0300

    API rest com fastify e comunicando com o banco

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,7 @@
 import { FastifyInstance, FastifyPluginOptions, FastifyRequest, FastifyReply } from "fastify";
 
-export async function routes(fastify: FastifyInstance, options: FastifyPluginOptions){
-    fastify.get("/teste",async (request:FastifyRequest, reply: FastifyReply) => {
+export async function routes(fastify: FastifyInstance, options: FastifyPluginOptions) {
+    fastify.get("/teste", async (request: FastifyRequest, reply: FastifyReply) => {
         return { ok: true }
     })
 }
--- src/server.ts
+++ src/server.ts
@@ -8,12 +8,11 @@ const start = async () => {
 
     await app.register(cors)
     await app.register(routes)
-    
+
     try {
-        await app.listen({port: 3333})
-    } catch(err) {
+        await app.listen({ port: 3333 })
+    } catch (err) {
         process.exit(1)
     }
 }
-
 start();
