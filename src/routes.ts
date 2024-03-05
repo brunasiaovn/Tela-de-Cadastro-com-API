5a01f8780a247ce178f5c3dbf592da07e58d6083
Bruna <bruna.dev@hotmail.com>
2024-03-05 11:30:59 -0300

    Cadastrando novo cliente com nome, email e status

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,13 @@
 import { FastifyInstance, FastifyPluginOptions, FastifyRequest, FastifyReply } from "fastify";
+import { CreateCustomerController } from "./controllers/CreateCustomerController";
 
 export async function routes(fastify: FastifyInstance, options: FastifyPluginOptions) {
+
     fastify.get("/teste", async (request: FastifyRequest, reply: FastifyReply) => {
         return { ok: true }
+    })
+
+    fastify.post("/customer", async (request: FastifyRequest, reply: FastifyReply) => {
+        return new CreateCustomerController().handle(request, reply)
     })
 }
