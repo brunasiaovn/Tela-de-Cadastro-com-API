08c5c97a8df9cc4a8fe6129d0cd37ba98a4349f3
Bruna <bruna.dev@hotmail.com>
2024-03-06 13:10:31 -0300

    Cadastrando um novo cliente

--- segundaparte/frontend/src/App.tsx
+++ segundaparte/frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef, FormEvent } from 'react'
 import { FiTrash } from 'react-icons/fi'
 import { api } from './services/api'
 
@@ -13,6 +13,9 @@ interface CustomerProps {
 export default function App() {
 
   const [customers, setCustomers] = useState<CustomerProps[]>([])
+  const nameRef = useRef<HTMLInputElement | null>(null)
+  const emailRef = useRef<HTMLInputElement| null>(null)
+
   useEffect(() => {
     loadCustomers();
   }, [])
@@ -22,23 +25,38 @@ export default function App() {
     setCustomers(response.data);
   }
 
+  async function handleSubimit(event: FormEvent){
+    event.preventDefault();
+
+    if(!nameRef.current?.value || !emailRef.current?.value) return;
+
+    const response = await api.post("/customer", {
+       name: nameRef.current?.value,
+       email: emailRef.current?.value
+    })
+
+    setCustomers(allCustomers => [...allCustomers, response.data])
+  }
+
   return (
     <div className="w-full min-h-screen bg-gray-900 flex justify-center px-4">
       <main className="m-10 w-full md:max-w-2xl ">
         <h1 className="text-4x1 font-medium text-white">Clientes</h1>
 
-        <form className="flex flex-col my-6">
+        <form className="flex flex-col my-6" onSubmit={handleSubimit}>
           <label className="font-medium text-white">Nome:</label>
           <input
             type="text"
             placeholder="Digite seu nome completo..."
             className="w-full mb-5 p-2 rounded"
+            ref={nameRef}
           />
           <label className="font-medium text-white">Email:</label>
           <input
             type="email"
             placeholder="Digite seu email..."
             className="w-full mb-5 p-2 rounded"
+            ref={emailRef}
           />
 
           <input type="submit"
